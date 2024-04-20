e7f1c832dd5c67b727c3fe67bbbc26163d2a5692
sipunkumarbiswal <140191421+sipunkumarbiswal@users.noreply.github.com>
2024-04-20 21:01:37 +0530

    Add files via upload

---
+++ protfolio/script.js
@@ -0,0 +1,11 @@
+function openMenu(){
+    document.getElementById("for-470px").style.display="block";
+    }
+
+    function closeMenu(){
+        document.getElementById("for-470px").style.display="none";
+    }
+
+    function closeA(){
+        document.getElementById("for-470px").style.display="none";
+    }
